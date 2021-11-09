c50728b2dd4906d9994cca1ecc3ca1d2c4fbdc55
luckymogale64 <57522837+luckymogale64@users.noreply.github.com>
2021-11-09 14:57:09 +0200

    for

--- srcipt.js
+++ srcipt.js
@@ -2,5 +2,5 @@ const names = ["happy", "tshepo", "kabelo","kgmotoni"];
 const product = document.getElementById("product");
 
 for (let i = 0; i < names.length; i++ ) {
-    product.innerHTML += "<br>" + names[i] + "<br>";
+    product.innerHTML += "<li>" + names[i] + "</li>";
 }
